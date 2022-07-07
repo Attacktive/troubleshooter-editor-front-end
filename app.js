2b140460a996a1745e269455503b9869d72d1d64
Attacktive <66462458+Attacktive@users.noreply.github.com>
2022-07-07 18:40:46 +0900

    Initial Implementation (#12)
    * Add favicon.ico
    
    * Add style.css
    
    * Start implementing
    
    * Fix error handling
    
    The arguments of the arrow function should NOT be reduced.
    https://stackoverflow.com/a/51826777/1986241
    
    * Replace ordinary functions with arrow functions
    
    * Change color hex value to lowercase

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const filesRouter = require("./routes/files");
 
 const app = express();
 
@@ -20,15 +20,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/files", filesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
 	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get("env") === "development"? err: {};
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-	res.render("index", { title: "Express" });
+router.get(
+	"/",
+	(req, res) => {
+		res.render("index", { title: "Troubleshooter Save File Editor" });
+	}
+);
+
+router.use((error, request, response, next) => {
+	if (error) {
+		console.log("request", request);
+		console.error(error);
+	}
+
+	next();
 });
 
 module.exports = router;
