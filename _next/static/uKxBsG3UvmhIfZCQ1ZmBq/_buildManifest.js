12dd1cd36469e711f7a71545747633c0be0ead83
Attacktive <Attacktive@users.noreply.github.com>
2022-07-17 13:58:32 +0000

    deploy: 88bd094b4f3d2e2633c4a0544d68dd53173ade67

---
+++ _next/static/uKxBsG3UvmhIfZCQ1ZmBq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-a79759374f77f4c2.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
