5b8982cfc624156050725842fee68e4fcbbc0900
Attacktive <welcome.hell.dude@gmail.com>
2022-07-17 22:31:00 +0900

    Update footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,13 +55,12 @@ const Home: NextPage = () => {
 
 			<footer className={styles.footer}>
 				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+					href="https://github.com/Attacktive/troubleshooter-editor"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Powered by{" "}
 					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
+						<Image src="/github.svg" alt="GitHub Logo" width={36} height={36}/>
 					</span>
 				</a>
 			</footer>
