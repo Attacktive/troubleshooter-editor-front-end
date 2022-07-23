ff88222ef568e295d64b9f9d5b40345d7b1b2b10
Attacktive <Attacktive@users.noreply.github.com>
2022-07-23 02:10:59 +0000

    deploy: 69880999277767f05229ea36921a3ce7582a5d71

---
+++ _next/static/-nfHWdWVxI0dT6AlNwgGb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed1896bce91d3987.css","static/chunks/pages/index-a257a508fc40dfd7.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/VY9LvmPL6vufvh4yynHix/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ed1896bce91d3987.css","static/chunks/pages/index-b014fb46836c60fc.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
