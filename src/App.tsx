050d824d02abd8f7dba9d989c8de842818cad54a
Attacktive <welcome.hell.dude@gmail.com>
2022-07-25 16:56:26 +0900

    Replace initial React main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useRef } from "react";
 import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
+	const fileForm = useRef<HTMLFormElement>();
+	const temporaryOutput = useRef<HTMLTextAreaElement>(null);
+	const styles = {
+		title: "",
+		footer: "",
+		logo: ""
+	};
+
+	function upload() {
+		const formData = new FormData(fileForm.current);
+
+		fetch(
+			"/files/upload",
+			{
+				method: "post",
+				body: formData
+			}
+		).then(response => {
+			console.log(response);
+			temporaryOutput.current!.value = response.toString();
+		}).catch(error => console.log(error));
+	}
+
+	function download() {
+		alert("TODO: download");
+	}
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo"/>
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
+		<div>
+			<Container fluid as={"main"}>
+				<header>
+					<title>Troubleshooter Editor</title>
+					<meta name="description" content="Troubleshooter Editor"/>
+					<link rel="icon" href="/favicon.ico"/>
+				</header>
+
+				<Row>
+					<Col>
+						<h1 className={styles.title}>
+							Troubleshooter Editor
+						</h1>
+					</Col>
+				</Row>
+				<Row as={"form"} ref={fileForm} className={"mt-4"}>
+					<Col xs={2}>
+						<input type={"file"} name={"file"} accept={".sav,.bak"}/>
+					</Col>
+					<Col xs={2}>
+						<button type={"button"} onClick={upload}>Upload</button>
+					</Col>
+					<Col>
+						<button type={"button"} onClick={download}>Save</button>
+					</Col>
+				</Row>
+				<Row className={"mt-4"}>
+					<Col>
+						<textarea ref={temporaryOutput} readOnly={true} style={{ width: "100%", minHeight: "300px" }}/>
+					</Col>
+				</Row>
+			</Container>
+
+			<footer className={styles.footer}>
 				<a
-					className="App-link"
-					href="https://reactjs.org"
+					href="https://github.com/Attacktive/troubleshooter-editor"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
 					Learn React
 				</a>
-			</header>
+			</footer>
 		</div>
 	);
 }
