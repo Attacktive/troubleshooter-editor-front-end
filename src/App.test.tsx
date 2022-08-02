1a502429a951c222c474d8cfac02ac4af44116a0
Attacktive <welcome.hell.dude@gmail.com>
2022-08-02 12:12:15 +0900

    Fix errors in test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,9 @@ import App from "./App";
 
 test("renders learn react link", () => {
 	render(<App/>);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+	const elements = screen.getAllByText(/Troubleshooter Editor/);
+	elements.forEach(element => {
+		console.log(element);
+		expect(element).toBeInTheDOM();
+	});
 });
