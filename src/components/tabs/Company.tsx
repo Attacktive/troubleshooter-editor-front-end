cec8878e8e3365a2601b52d6c584a8c7693bce69
Attacktive <welcome.hell.dude@gmail.com>
2022-12-09 16:43:40 +0900

    Introduce 'react-hook-form'

--- src/components/tabs/Company.tsx
+++ src/components/tabs/Company.tsx
@@ -1,28 +1,37 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Col, FormControl, FormGroup, FormLabel, FormSelect, Row } from "react-bootstrap";
 import { CompanyInfo } from "../../types";
 
 export default function Company({ company }: { company?: CompanyInfo }) {
+	const { register, setValue } = useForm<CompanyInfo>();
+
 	const dataFetched = (company && company.id > 0);
+	if (dataFetched) {
+		setValue("id", company.id);
+		setValue("name", company.name);
+		setValue("vill", company.vill);
+		setValue("properties", company.properties);
+	}
 
 	return (
 		<Row>
 			<Col>
 				<FormGroup>
 					<FormLabel>ID</FormLabel>
-					<FormControl type={"number"} defaultValue={company?.id} readOnly={true}/>
+					<FormControl type={"number"} { ...register("id") } readOnly={true}/>
 				</FormGroup>
 				<FormGroup>
 					<FormLabel>Name</FormLabel>
-					<FormControl type={"text"} defaultValue={company?.name} readOnly={!dataFetched}/>
+					<FormControl type={"text"} { ...register("name") } readOnly={!dataFetched}/>
 				</FormGroup>
 				<FormGroup>
 					<FormLabel>Vill</FormLabel>
-					<FormControl type={"number"} defaultValue={company?.vill} min={0} step={1} readOnly={!dataFetched}/>
+					<FormControl type={"number"} { ...register("vill") } min={0} step={1} readOnly={!dataFetched}/>
 				</FormGroup>
 				<FormGroup>
 					<FormLabel>Difficulty</FormLabel>
-					<FormSelect defaultValue={company?.properties["GameDifficulty"]} disabled={!dataFetched}>
+					<FormSelect { ...register("properties.GameDifficulty") } disabled={!dataFetched}>
 						<option value={"None"} disabled={true}>Choose one</option>
 						<option value={"Story"}>Story</option>
 						<option value={"Safty"}>Safety</option>
