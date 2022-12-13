cd73f495e3098f237e3e48c8742f89f41442f0de
Attacktive <welcome.hell.dude@gmail.com>
2022-12-13 11:15:51 +0900

    Fix form handling in component 'Company'
    The rest of it is still under construction.

--- src/components/tabs/Company.tsx
+++ src/components/tabs/Company.tsx
@@ -1,37 +1,66 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useEffect, useState } from "react";
 import { Col, FormControl, FormGroup, FormLabel, FormSelect, Row } from "react-bootstrap";
 import { CompanyInfo } from "../../types";
 
-export default function Company({ company }: { company?: CompanyInfo }) {
-	const { register, setValue } = useForm<CompanyInfo>();
+export default function Company({ company }: { company: CompanyInfo }) {
+	const [formData, setFormData] = useState<CompanyInfo>({
+		id: 0,
+		name: "",
+		vill: -1,
+		properties: { GameDifficulty: "None" }
+	});
 
-	const dataFetched = (company && company.id > 0);
-	if (dataFetched) {
-		setValue("id", company.id);
-		setValue("name", company.name);
-		setValue("vill", company.vill);
-		setValue("properties", company.properties);
+	const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => onFormControlChange(event);
+	const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => onFormControlChange(event);
+	const onFormControlChange = (event: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+		const { name, value } = event.target;
+
+		const newData = {
+			...formData,
+			[name]: value
+		};
+
+		// TODO: Ewwww. Any alternatives?
+		if (name === "properties.GameDifficulty") {
+			newData.properties.GameDifficulty = value;
+		}
+
+		setFormData(newData);
 	}
+
+	const dataFetched = (company.id > 0);
+	useEffect(
+		() => {
+		if (dataFetched) {
+				setFormData({
+					id: company.id,
+					name: company.name,
+					vill: company.vill,
+					properties: company.properties
+				});
+			}
+		},
+		[company]
+	);
 
 	return (
 		<Row>
 			<Col>
 				<FormGroup>
 					<FormLabel>ID</FormLabel>
-					<FormControl type={"number"} { ...register("id") } readOnly={true}/>
+					<FormControl type="number" name="id" value={formData.id} readOnly={true}/>
 				</FormGroup>
 				<FormGroup>
 					<FormLabel>Name</FormLabel>
-					<FormControl type={"text"} { ...register("name") } readOnly={!dataFetched}/>
+					<FormControl type="text" name="name" value={formData.name} readOnly={!dataFetched} onChange={onInputChange}/>
 				</FormGroup>
 				<FormGroup>
 					<FormLabel>Vill</FormLabel>
-					<FormControl type={"number"} { ...register("vill") } min={0} step={1} readOnly={!dataFetched}/>
+					<FormControl type="number" name="vill" value={formData.vill} min={0} step={1} readOnly={!dataFetched} onChange={onInputChange}/>
 				</FormGroup>
 				<FormGroup>
 					<FormLabel>Difficulty</FormLabel>
-					<FormSelect { ...register("properties.GameDifficulty") } disabled={!dataFetched}>
+					<FormSelect name="properties.GameDifficulty" value={formData.properties.GameDifficulty} disabled={!dataFetched} onChange={onSelectChange}>
 						<option value={"None"} disabled={true}>Choose one</option>
 						<option value={"Story"}>Story</option>
 						<option value={"Safty"}>Safety</option>
