e2a428b1dece03d19c1edc660ee14e942b6ec300
Attacktive <tribute2pantera@gmail.com>
2023-12-09 15:36:35 +0900

    fix: replace dom binding with value binding
    https://github.com/Attacktive/troubleshooter-editor-front-end/issues/90

--- src/components/tabs/Company.tsx
+++ src/components/tabs/Company.tsx
@@ -1,64 +1,28 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { Button, Col, FormControl, FormGroup, FormLabel, FormSelect, Row } from "react-bootstrap";
 import { CompanyInfo } from "../../types";
 
-export default function Company({ initialCompany }: { initialCompany: CompanyInfo }) {
-	const [ignored, setFormData] = useState<CompanyInfo>({
-		id: 0,
-		name: "",
-		vill: -1,
-		properties: { GameDifficulty: "None" }
-	});
-
-	const id = useRef<HTMLInputElement>(null);
-	const name = useRef<HTMLInputElement>(null);
-	const vill = useRef<HTMLInputElement>(null);
-	const difficulty = useRef<HTMLSelectElement>(null);
-
-	const dataFetched = (initialCompany.id > 0);
-	useEffect(
-		() => {
-			if (dataFetched) {
-				setFormData({
-					id: initialCompany.id,
-					name: initialCompany.name,
-					vill: initialCompany.vill,
-					properties: initialCompany.properties
-				});
-			}
-		},
-		[dataFetched, initialCompany]
-	);
-
-	const onApply = () => {
-		if (id.current && name.current && vill.current && difficulty.current) {
-			setFormData({
-				id: parseInt(id.current.value),
-				name: name.current.value,
-				vill: parseInt(vill.current.value),
-				properties: { GameDifficulty: difficulty.current.value }
-			});
-		}
-	};
+export default function Company({ company }: { company: CompanyInfo }) {
+	const onApply = () => console.log("onApply");
 
 	return (
 		<Row>
 			<Col>
 				<FormGroup>
 					<FormLabel>ID</FormLabel>
-					<FormControl ref={id} type="number" name="id" defaultValue={initialCompany.id} key={initialCompany.id} readOnly={true}/>
+					<FormControl type="number" name="id" defaultValue={company.id} key={company.id} readOnly/>
 				</FormGroup>
 				<FormGroup className="mt-2">
 					<FormLabel>Name</FormLabel>
-					<FormControl ref={name} type="text" name="name" defaultValue={initialCompany.name} key={initialCompany.name} readOnly={!dataFetched}/>
+					<FormControl type="text" name="name" defaultValue={company.name} key={company.name}/>
 				</FormGroup>
 				<FormGroup className="mt-2">
 					<FormLabel>Vill</FormLabel>
-					<FormControl ref={vill} type="number" name="vill" defaultValue={initialCompany.vill} min={0} step={1} key={initialCompany.vill} readOnly={!dataFetched}/>
+					<FormControl type="number" name="vill" defaultValue={company.vill} min={0} step={1} key={company.vill}/>
 				</FormGroup>
 				<FormGroup className="mt-2">
 					<FormLabel>Difficulty</FormLabel>
-					<FormSelect ref={difficulty} name="properties.GameDifficulty" defaultValue={initialCompany.properties.GameDifficulty} key={initialCompany.properties.GameDifficulty} disabled={!dataFetched}>
+					<FormSelect name="properties.GameDifficulty" defaultValue={company.properties.GameDifficulty} key={company.properties.GameDifficulty}>
 						<option value={"None"} disabled={true}>Choose one</option>
 						<option value={"Story"}>Story</option>
 						<option value={"Safty"}>Safety</option>
--- src/components/tabs/Rosters.tsx
+++ src/components/tabs/Rosters.tsx
@@ -1,79 +1,40 @@
-import React, { createRef, useEffect, useRef, useState } from "react";
+import React from "react";
 import { Accordion, Button, Col, FormControl, FormGroup, FormLabel, Row } from "react-bootstrap";
-import { RosterCollection, RosterInfo } from "../../types";
+import { RosterCollection } from "../../types";
 import AccordionItem from "react-bootstrap/AccordionItem";
 import AccordionHeader from "react-bootstrap/AccordionHeader";
 import AccordionBody from "react-bootstrap/AccordionBody";
 
-export default function Roster({ initialRosters }: { initialRosters: RosterCollection }) {
-	const [rosters, setRosters] = useState<RosterCollection>(Array.from(initialRosters));
-	const refs = useRef(rosters.map(() => {
-		return {
-			id: createRef<HTMLInputElement>(),
-			name: createRef<HTMLInputElement>(),
-			class: createRef<HTMLInputElement>(),
-			level: createRef<HTMLInputElement>(),
-			exp: createRef<HTMLInputElement>()
-		};
-	}));
-
-	useEffect(
-		() => {
-			if (initialRosters.length > 0) {
-				setRosters(initialRosters);
-			}
-		},
-		[initialRosters]
-	);
-
-	const onApply = () => {
-		if (refs.current) {
-			const newRosters: RosterCollection = refs.current
-				.filter(ref => ref.id.current && ref.name.current && ref.class.current && ref.level.current && ref.exp.current)
-				.map(ref => {
-					const roster: RosterInfo = {
-						id: parseInt(ref.id.current!!.value),
-						name: ref.name.current!!.value,
-						class: ref.class.current!!.value,
-						level: parseInt(ref.level.current!!.value),
-						exp: parseInt(ref.exp.current!!.value),
-						properties: {}
-					};
-
-					return roster;
-				});
-
-			setRosters(newRosters);
-		}
-	};
+export default function Roster({ rosters }: { rosters: RosterCollection }) {
+	const onApply = () => console.log("onApply");
 
 	return (
 		<Row>
 			<Col>
 				<Accordion>
 					{
-						initialRosters.map((roster, index) => {
+						rosters.map(roster => {
 							return (
 								<AccordionItem key={`${roster.id}`} eventKey={`${roster.id}`}>
 									<AccordionHeader>#{roster.id} {roster.name}</AccordionHeader>
 									<AccordionBody>
-										<input type="hidden" ref={refs.current[index].id}/>
+										<input type="hidden"/>
 										<FormGroup>
 											<FormLabel>Name</FormLabel>
-											<FormControl type="text" ref={refs.current[index].name} defaultValue={roster.name}/>
+											<FormControl type="text" defaultValue={roster.name}/>
 										</FormGroup>
 										<FormGroup>
 											<FormLabel>Class</FormLabel>
 											{/* TODO: change it to a select maybe */}
-											<FormControl type="text" ref={refs.current[index].class} defaultValue={roster.class}/>
+											<FormControl type="text" defaultValue={roster.class}/>
 										</FormGroup>
 										<FormGroup>
 											<FormLabel>Level</FormLabel>
-											<FormControl type="number" ref={refs.current[index].level} defaultValue={roster.level} min={0} step={1}/>
+											<FormControl type="number" defaultValue={roster.level} min={0} step={1}/>
 										</FormGroup>
 										<FormGroup>
 											<FormLabel>Exp</FormLabel>
-											<FormControl type="number" ref={refs.current[index].exp} defaultValue={roster.exp} min={0} step={1}/>
+											<FormControl type="number" defaultValue={roster.exp} min={0} step={1}/>
 										</FormGroup>
 										{/* TODO: handle properties */}
 										{JSON.stringify(roster.properties)}
