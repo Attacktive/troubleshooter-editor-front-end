36baebdb276c33bdd389f1ab9f62bf8ec4955362
Attacktive <tribute2pantera@gmail.com>
2023-12-21 15:49:21 +0900

    refactor: make `vite.config.js` more compact

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(() => {
-	return {
-		plugins: [
-			react(),
-			viteTsconfigPaths()
-		],
-		test: {
-			globals: true,
-			environment: "jsdom",
-			setupFiles: ["./src/test/setup.ts"]
-		}
-	};
-});
+export default defineConfig(() => ({
+	plugins: [
+		react(),
+		viteTsconfigPaths()
+	],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: ["./src/test/setup.ts"]
+	}
+}));
