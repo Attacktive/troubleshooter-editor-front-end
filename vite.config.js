4f1b2fbd43d40058caaafa1f719a91b188ff2aab
Attacktive <tribute2pantera@gmail.com>
2023-12-21 16:13:30 +0900

    Revert "refactor: remove redundant vite configurations"
    This reverts commit d93bcc8a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => ({
+	build: {
+		outDir: "build"
+	},
 	plugins: [
 		react(),
 		viteTsconfigPaths()
