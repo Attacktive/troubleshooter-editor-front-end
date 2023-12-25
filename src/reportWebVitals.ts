e359960170cf45a22e87c2f7695306c0be89050e
Attacktive <tribute2pantera@gmail.com>
2023-12-25 14:21:03 +0900

    refactor: log web vitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,13 +1,11 @@
-import { ReportHandler } from "web-vitals";
-
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-	if (onPerfEntry && onPerfEntry instanceof Function) {
-		import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-			getCLS(onPerfEntry);
-			getFID(onPerfEntry);
-			getFCP(onPerfEntry);
-			getLCP(onPerfEntry);
-			getTTFB(onPerfEntry);
+const reportWebVitals = (reportCallback?: (any: any) => void) => {
+	if (reportCallback) {
+		import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
+			onCLS(reportCallback);
+			onFID(reportCallback);
+			onFCP(reportCallback);
+			onLCP(reportCallback);
+			onTTFB(reportCallback);
 		});
 	}
 };
